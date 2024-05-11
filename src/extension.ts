e77f531a9d08e26314245deba69cd1aa5baba6e5
alejlatorre <alej669@gmail.com>
2024-05-11 04:56:19 -0500

    feature: add file scheme checker and derive a new uri

--- src/extension.ts
+++ src/extension.ts
@@ -92,6 +92,20 @@ class FileBrowser {
         this.current.show();
     }
 
+    async getStat(uri: Uri): Promise<vscode.FileStat | null> {
+        try {
+            if (uri.scheme === "file") {
+                return await vscode.workspace.fs.stat(uri);
+            } else {
+                const newUri = uri.with({ scheme: "file", fragment: '' });
+                this.path = new Path(newUri);
+                return await vscode.workspace.fs.stat(newUri);
+            }
+        } catch (err) {
+            return null;
+        }
+    }
+
     async update() {
         // FIXME: temporary and UGLY fix of https://github.com/bodil/vscode-file-browser/issues/35.
         // Brought in from here https://github.com/atariq11700/vscode-file-browser/commit/a2525d01f262f17dac2c478e56640c9ce1f65713.
@@ -101,7 +115,7 @@ class FileBrowser {
         this.current.title = this.path.fsPath;
         this.current.value = "";
 
-        const stat = (await Result.try(vscode.workspace.fs.stat(this.path.uri))).unwrap();
+        const stat = await this.getStat(this.path.uri);
         if (stat && this.inActions && (stat.type & FileType.File) === FileType.File) {
             this.items = [
                 action("$(file) Open this file", Action.OpenFile),
